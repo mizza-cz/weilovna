bd959f1b81e04b247312539fc0c8ba32d26ff7af
mizza-cz <mizza.cz666@gmail.com>
2024-07-11 13:15:53 +0200

    add fixes

--- src/components/archiveProducts/archive-section.js
+++ src/components/archiveProducts/archive-section.js
@@ -1,24 +1,28 @@
-const archProducts = document.querySelectorAll(
-  ".archiveProducts__inner .col-lg-3"
-);
-const archBtn = document.querySelector(".archiveProducts__btn");
-let archCount = 12;
+const archInner = document.querySelector(".archiveProducts__inner");
 
-function showProd() {
-  archProducts.forEach((product, index) => {
-    if (index < archCount) {
-      product.classList.add("visible");
+if (archInner) {
+  const archProducts = document.querySelectorAll(
+    ".archiveProducts__inner .col-lg-3"
+  );
+  const archBtn = document.querySelector(".archiveProducts__btn");
+  let archCount = 12;
+
+  function showProd() {
+    archProducts.forEach((product, index) => {
+      if (index < archCount) {
+        product.classList.add("visible");
+      }
+    });
+
+    if (archCount >= archProducts.length) {
+      archBtn.style.display = "none";
     }
+  }
+
+  archBtn.addEventListener("click", function () {
+    archCount += 4;
+    showProd();
   });
 
-  if (archCount >= archProducts.length) {
-    archBtn.style.display = "none";
-  }
-}
-
-archBtn.addEventListener("click", function () {
-  archCount += 4;
   showProd();
-});
-
-showProd();
+}
--- src/components/aside/aside.js
+++ src/components/aside/aside.js
@@ -1,22 +1,26 @@
-const products = document.querySelectorAll(".asideProduct");
-const buttonMore = document.querySelector(".asideBtn");
-let visibleCount = 3;
+const asideProductsContainer = document.querySelector(".aside__products");
 
-function showProducts() {
-  products.forEach((product, index) => {
-    if (index < visibleCount) {
-      product.classList.add("visible");
+if (asideProductsContainer) {
+  const products = document.querySelectorAll(".asideProduct");
+  const buttonMore = document.querySelector(".asideBtn");
+  let visibleCount = 3;
+
+  function showProducts() {
+    products.forEach((product, index) => {
+      if (index < visibleCount) {
+        product.classList.add("visible");
+      }
+    });
+
+    if (visibleCount >= products.length) {
+      buttonMore.style.display = "none";
     }
+  }
+
+  buttonMore.addEventListener("click", function () {
+    visibleCount += 3;
+    showProducts();
   });
 
-  if (visibleCount >= products.length) {
-    buttonMore.style.display = "none";
-  }
-}
-
-buttonMore.addEventListener("click", function () {
-  visibleCount += 3;
   showProducts();
-});
-
-showProducts();
+}

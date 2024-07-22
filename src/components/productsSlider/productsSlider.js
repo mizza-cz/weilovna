fe312e07d9ede4536069701d18d07df98eca6ee1
mizza-cz <mizza.cz666@gmail.com>
2024-07-22 14:30:29 +0200

    fix hp

--- src/components/productsSlider/productsSlider.js
+++ src/components/productsSlider/productsSlider.js
@@ -1,69 +1,109 @@
+// $(document).ready(function () {
+//   $(".productsSlider__inner").each(function () {
+//     var $slider = $(this);
+//     var initialized = false;
+
+//     var sliderSettings = {
+//       slidesToShow: 4,
+//       slidesToScroll: 1,
+//       prevArrow:
+//         '<button class="productsSlider__btn productsSlider__btnprev"><img src="images/ico/left.svg" loading="lazy" alt="" /></button>',
+//       nextArrow:
+//         '<button class="productsSlider__btn productsSlider__btnnext"><img src="images/ico/right.svg" loading="lazy" alt="" /></button>',
+//       responsive: [
+//         {
+//           breakpoint: 920,
+//           settings: {
+//             slidesToShow: 4,
+//             dots: true,
+//             arrows: false,
+//           },
+//         },
+//         {
+//           breakpoint: 640,
+//           settings: {
+//             slidesToShow: 2,
+//             dots: true,
+//             arrows: false,
+//             centerMode: true,
+//           },
+//         },
+//         {
+//           breakpoint: 520,
+//           settings: {
+//             slidesToShow: 1,
+//             dots: true,
+//             arrows: false,
+//             centerMode: true,
+//             centerPadding: "25%",
+//           },
+//         },
+//       ],
+//     };
+
+//     function checkAndInitializeSlider() {
+//       var elementCount = $slider.children(".productItem").length;
+
+//       if (
+//         !initialized &&
+//         (elementCount > 4 || (elementCount <= 4 && $(window).width() < 1001))
+//       ) {
+//         $slider.slick(sliderSettings);
+//         initialized = true;
+//       } else if (
+//         initialized &&
+//         elementCount <= 4 &&
+//         $(window).width() >= 1001
+//       ) {
+//         $slider.slick("unslick");
+//         initialized = false;
+//       }
+//     }
+
+//     checkAndInitializeSlider();
+
+//     $(window).on("resize", function () {
+//       checkAndInitializeSlider();
+//     });
+//   });
+// });
+
 $(document).ready(function () {
-  $(".productsSlider__inner").each(function () {
-    var $slider = $(this);
-    var initialized = false;
-
-    var sliderSettings = {
-      slidesToShow: 4,
-      slidesToScroll: 1,
-      prevArrow:
-        '<button class="productsSlider__btn productsSlider__btnprev"><img src="images/ico/left.svg" loading="lazy" alt="" /></button>',
-      nextArrow:
-        '<button class="productsSlider__btn productsSlider__btnnext"><img src="images/ico/right.svg" loading="lazy" alt="" /></button>',
-      responsive: [
-        {
-          breakpoint: 920,
-          settings: {
-            slidesToShow: 4,
-            dots: true,
-            arrows: false,
-          },  
+  $(".productsSlider__inner").slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    prevArrow:
+      '<button class="productsSlider__btn productsSlider__btnprev"><img src="images/ico/left.svg" loading="lazy" alt="" /></button>',
+    nextArrow:
+      '<button class="productsSlider__btn productsSlider__btnnext"><img src="images/ico/right.svg" loading="lazy" alt="" /></button>',
+    responsive: [
+      {
+        breakpoint: 920,
+        settings: {
+          slidesToShow: 4,
+          dots: true,
+          arrows: false,
         },
-        {
-          breakpoint: 640,
-          settings: {
-            slidesToShow: 2,
-            dots: true,
-            arrows: false,
-            centerMode: true,
-          },
+      },
+      {
+        breakpoint: 640,
+        settings: {
+          slidesToShow: 2,
+          dots: true,
+          arrows: false,
+          centerMode: true,
         },
-        {
-          breakpoint: 520,
-          settings: {
-            slidesToShow: 1,
-            dots: true,
-            arrows: false,
-            centerMode: true,
-            centerPadding: "25%",
-          },
+      },
+      {
+        breakpoint: 520,
+        settings: {
+          slidesToShow: 1,
+          dots: true,
+          arrows: false,
+          centerMode: true,
+          centerPadding: "25%",
         },
-      ],
-    };
-
-    function checkAndInitializeSlider() {
-      var elementCount = $slider.children(".productItem").length;
-
-      if (
-        !initialized &&
-        (elementCount > 4 || (elementCount <= 4 && $(window).width() < 1001))
-      ) {
-        $slider.slick(sliderSettings);
-        initialized = true;
-      } else if (
-        initialized &&
-        elementCount <= 4 &&
-        $(window).width() >= 1001
-      ) {
-        $slider.slick("unslick");
-        initialized = false;
-      }
-    }
-
-    checkAndInitializeSlider();
-
-    $(window).on("resize", function () {
-      checkAndInitializeSlider();
-    });
+      },
+    ],
   });
 });
